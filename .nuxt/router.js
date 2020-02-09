a5ce9aab39ca7bdb227855d02e6bfb176a338d0b
ipung anwar <ipunganwar@ipungs-MacBook-Air.local>
2020-02-09 23:27:02 +0700

    git blog

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,8 +3,8 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _8baa77ee = () => interopDefault(import('../pages/blog/_slug.vue' /* webpackChunkName: "pages/blog/_slug" */))
-const _3697d4d6 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _934c8da6 = () => interopDefault(import('../pages/blog/_slug.vue' /* webpackChunkName: "pages/blog/_slug" */))
+const _27b58d9c = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -24,11 +24,11 @@ export const routerOptions = {
 
   routes: [{
     path: "/blog/:slug?",
-    component: _8baa77ee,
+    component: _934c8da6,
     name: "blog-slug"
   }, {
     path: "/",
-    component: _3697d4d6,
+    component: _27b58d9c,
     name: "index"
   }],
 
